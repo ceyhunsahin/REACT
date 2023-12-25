33d0873a7d17a0cfd51894235b36d6049c235444
ceyhunsahin <csahin17072010@gmail.com>
2023-12-25 13:24:41 +0100

    rc02_jsx_components

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -2,16 +2,33 @@ import React from "react";
 import Card from "./components/Card";
 import { contacts } from "./data";
 import "./styles.css";
-
+/* import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"; */
 
 export default function App() {
   return (
+/*     <Router>
+    <Link to= ""></Link>
+    <Switch>
+      <Route path="">
+
+      </Route>
+    </Switch>
+
+    </Router> */
+
     <div className="App">
       <h1 className="heading">My Contacts</h1>
       {contacts.map((contact) => (
         // every element should have uniq key
         <Card key={contact.id} contact={contact} />
       ))}
+
     </div>
   );
 }
+
