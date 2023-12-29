ab2f95168c515bb89b1b16428ded76666b7b66eb
ceyhunsahin <csahin17072010@gmail.com>
2023-12-29 19:45:30 +0100

    class Components-DidMount, DidUpdate,WillUnmount

--- rc05_componentlifecyscle_hooks/src/App.js
+++ rc05_componentlifecyscle_hooks/src/App.js
@@ -1,11 +1,16 @@
 import ClassComponents from './components/ClassComponents';
 import './App.css';
+import React, { useState } from'react';
 
 function App() {
+  const [isVisible, setIsVisible] =useState(true);
+  
+
   return (
     <div className="App">
       <h2> LifeCycles</h2>
-      <ClassComponents />
+      {isVisible && <ClassComponents />}
+      <button onClick={() => setIsVisible(!isVisible)}> Toggle </button>
     </div>
   );
 }
--- rc05_componentlifecyscle_hooks/src/components/ClassComponents.jsx
+++ rc05_componentlifecyscle_hooks/src/components/ClassComponents.jsx
@@ -1,17 +1,59 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+
 
 export default class ClassComponents extends Component {
-    constructor (props) {
+    /* constructor (props) {
         super(props)
-        this.state = {}
-    }
+        this.state = {count : 0}
+    } */
+  state = {count : 0}
+
+  increment = () => {
+    this.setState({count : this.state.count + 1})
+  }
+
+  decrement = () => {
+    this.setState({count : this.state.count - 1})
+  }
+
+  nameWrite = () => {
+    this.setState({name : "Ceyhun"})
+  }
+
+/*   componentDidMount = () => {
+    console.log("ClassComponents componentDidMount  mounted")
+    this.increment()
+
+  } */
+  componentDidUpdate = (prevProps, prevState) => { 
+    console.log("ClassComponents componentDidUpdate  mounted")
+    console.log("prevState", prevState)
+
+   }
+   getSnapshotBeforeUpdate = (prevProps, prevState) => {
+    console.log("ClassComponents getSnapshotBeforeUpdate  mounted")
+    console.log("prevState", prevState)
+    return prevState
+   }
+
+   componentWillUnmount = () => {   
+        console.log("ClassComponents componentWillUnmount  mounted")
+   }
+
+  //once Render then componenDidMount Works
 
   render() {
+    console.log("ClassComponents componentDidUpdate rendered")
+
     return (
       <div>
 
             <h2>Class Components</h2>
+            <h3>Count : {this.state.count}</h3>
+            <h3>Name : {this.state.name}</h3>
+            <button onClick={this.increment}>Increment</button>
+            <button onClick={this.decrement}>Decrement</button>
+            <button onClick={this.nameWrite}>Name</button>
         
       </div>
     )
