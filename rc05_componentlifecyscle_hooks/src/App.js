751fcbc62082b3b9e204e64701cebdb3a91b438f
ceyhunsahin <csahin17072010@gmail.com>
2023-12-29 15:57:22 +0100

    rc05_componentlifecyscle_hooks

---
+++ rc05_componentlifecyscle_hooks/src/App.js
@@ -0,0 +1,13 @@
+import ClassComponents from './components/ClassComponents';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2> LifeCycles</h2>
+      <ClassComponents />
+    </div>
+  );
+}
+
+export default App;
---
+++ rc05_componentlifecyscle_hooks/src/components/ClassComponents.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class ClassComponents extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {}
+    }
+
+  render() {
+    return (
+      <div>
+
+            <h2>Class Components</h2>
+        
+      </div>
+    )
+  }
+}
