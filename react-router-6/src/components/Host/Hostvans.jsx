6c86dd878796cf99b6159e90598bb2e63b604972
ceyhunsahin <csahin17072010@gmail.com>
2024-01-10 23:57:02 +0100

    loader intro

--- react-router-6/src/components/Host/Hostvans.jsx
+++ react-router-6/src/components/Host/Hostvans.jsx
@@ -1,49 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import styles from  "./Host.module.css"
-import {Link} from "react-router-dom";
-import {getData} from "../api";
+import React, { useEffect, useState } from "react";
+import styles from "./Host.module.css";
+import { Link } from "react-router-dom";
+import { getData } from "../api";
 
 function Hostvans() {
-    const [vans, setVans] = useState([])
-    const [loading, setLoading] = useState(false);
+  const [vans, setVans] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        const fetchdata = async () => {
-            setLoading(true);
-            const data = await getData();
-            setTimeout(() => {
-                setVans(data);
-                setLoading(false);
-            }, 1000);
-            
-        }
-        fetchdata();
-        
-    }, [])
+  useEffect(() => {
+    const fetchdata = async () => {
+      setLoading(true);
+      const data = await getData();
+      setTimeout(() => {
+        setVans(data);
+        setLoading(false);
+      }, 1000);
+    };
+    fetchdata();
+  }, []);
 
-    if (loading) {
-        return <h1>Loading...</h1>
-    }
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
 
-
-    return (
-        <div style = {{margin : "10px"}}>
-            <h2>Your Listed Vans </h2>
-            <div className>
-                {vans.map((each) => (
-                    <Link to = {`${each.id}`} key = {each.id} 
-                    className = {styles.miniCard}  >
-                        <img className = {styles.image} src={each.image} alt={each.title} />
-                        <div className = {styles.miniCardSecond}>
-                            <h3>{each.title}</h3>
-                            <p>{each.price}$</p>
-                        </div>
-                    </Link>
-                ))}
+  return (
+    <div style={{ margin: "10px" }}>
+      <h2>Your Listed Vans </h2>
+      <div className>
+        {vans.map((each) => (
+          <Link to={`${each.id}`} key={each.id} className={styles.miniCard}>
+            <img className={styles.image} src={each.image} alt={each.title} />
+            <div className={styles.miniCardSecond}>
+              <h3>{each.title}</h3>
+              <p>{each.price}$</p>
             </div>
-
-        </div>
-    )
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Hostvans
+export default Hostvans;
--- react-router-6/src/components/Vans/VansDetail.jsx
+++ react-router-6/src/components/Vans/VansDetail.jsx
@@ -1,58 +1,55 @@
-import React, {useEffect, useState} from 'react'
-import {useParams, useNavigate, useLocation, Link} from "react-router-dom"
-import BackArrow from './BackArrow';
-
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate, useLocation, Link } from "react-router-dom";
+import BackArrow from "./BackArrow";
 
 export default function VansDetail() {
-    const idOfProduct = useParams();
-    console.log("idOfProduct", idOfProduct)
+  const idOfProduct = useParams();
+  console.log("idOfProduct", idOfProduct);
 
-    const [product, setProduct] = useState([])
-    const location = useLocation();
-    console.log("location", location)
+  const [product, setProduct] = useState([]);
+  const location = useLocation();
+  console.log("location", location);
 
-    useEffect(() => {
-        fetch(`https://fakestoreapi.com/products/${idOfProduct.id}`)
-     .then(res => res.json())
-     .then(data => setProduct(data))
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${idOfProduct.id}`)
+      .then((res) => res.json())
+      .then((data) => setProduct(data));
+  }, [idOfProduct.id]);
 
-    },[idOfProduct.id])
+  console.log("product", product);
 
-    console.log("product", product)
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const handleClick = () => {
+    navigate(-1);
+  };
 
-    const handleClick = () => {
-        navigate(-1)
-    }
+  const search = location.state?.search || "";
+  const title = location.state?.title || "all";
 
-    const search = location.state?.search || ""
-    const title = location.state?.title || "all"
-
-
-    return (
+  return (
+    <div>
+      <BackArrow onClick={handleClick} />
+      <Link
+        //to="../.."
+        //to = {`../..${location.state.search}`} // more consistent
+        //but if location.state.search is nul, it will be messy
+        to={`../..${search}`}
+        relative="path"
+      >
+        Back to {title}
+      </Link>
+      {idOfProduct ? (
         <div>
-        <BackArrow onClick = {handleClick}/> 
-        <Link 
-            //to="../.."
-            //to = {`../..${location.state.search}`} // more consistent 
-            //but if location.state.search is nul, it will be messy
-            to = {`../..${search}`}
-            relative='path'
-            >Back to {title}
-            </Link>
-        {idOfProduct ?
-        (<div>
-            <h1>{product.title}</h1>
-            <img src={product.image} alt={product.title} /> 
-            <p>{product.description}</p>
-        </div>)
-            :
-            (<div>
-            <p>Loading...</p>
-            </div>)
-        }
-            
+          <h1>{product.title}</h1>
+          <img src={product.image} alt={product.title} />
+          <p>{product.description}</p>
         </div>
-    )
+      ) : (
+        <div>
+          <p>Loading...</p>
+        </div>
+      )}
+    </div>
+  );
 }
